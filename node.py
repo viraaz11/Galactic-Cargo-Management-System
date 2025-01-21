377d5fb1ca2a8847b175e9a6b86ae94ce481c2dc
viraaz11 <155564768+viraaz11@users.noreply.github.com>
2025-01-21 14:41:01 +0530

    Revert "codes"
    This reverts commit 163a4ab2996b778e5a8bfa16eddb350d9511afc9.

--- node.py
+++
@@ -1,8 +0,0 @@
-
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value  
-        self.left = None
-        self.right = None
-        self.height = 1
